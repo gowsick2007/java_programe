11a12c017dcf2314f692643761c35a6ba49744ce
gowsick2007 <you@example.com>
2025-02-26 20:58:19 +0530

    arithmetic

---
+++ arithmetic.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; 
+
+ class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+    
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        
+
+        
+        System.out.println("a + b = " + (a + b));
+  
+  
+      System.out.println("a - b = " + (a - b));
+  
+      
+      System.out.println("a * b = " + (a * b));
+  
+      
+      System.out.println("a / b = " + (a / b));
+  
+  
+      System.out.println("a % b = " + (a % b));
+
+ }
+}
